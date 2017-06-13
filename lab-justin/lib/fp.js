0600ee2d7131a0cfa4b509c186774d77482b2fb7
Justin Green <justin98366@gmail.com>
2017-06-13 15:14:52 -0700

    got a lot of my assignment done

---
+++ lab-justin/lib/fp.js
@@ -0,0 +1,25 @@
+'use strict';
+
+
+const lists = module.exports = {};
+
+lists.map = (list, callback) => {
+  return Array.prototype.map.call(list, callback);
+};
+
+
+lists.reduce = (list, callback) => {
+  return Array.prototype.reduce.apply(list, callback);
+};
+
+lists.filter = (list, callback) => {
+  return Array.prototype.filter.call(list, callback);
+};
+
+lists.concat = (list, callback) => {
+  return Array.prototype.concat.apply(list, callback);
+};
+
+lists.splice = (list, callback) => {
+  return Array.prototype.splice.apply(list, callback);
+};
